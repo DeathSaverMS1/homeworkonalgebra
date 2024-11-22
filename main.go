8d7c21c8d09ba03d3fb6a717636920c5c17235ec
DeathSaverMS1 <manvikg23@gmail.com>
2024-11-21 22:27:24 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "https://kazwire.com/", nil
+            return "https://holyunblocker.org/", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
