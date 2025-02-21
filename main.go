804cccdeeddcea8a8d29bcda498ab7175d554a2b
DeathSaverMS1 <manvikg23@gmail.com>
2025-02-21 09:27:40 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
-            return "https://rocketbotroyale.winterpixel.io/", nil
+            return "https://kazwire.com/", nil
         },
     })
     http.ListenAndServe(":"+os.Getenv("PORT"), p)
